910163a8b2e87b2ebceb7027f2dfd8205de28094
Widua <mikolaj.widla@gmail.com>
2021-07-24 16:53:34 +0200

    Added bubblesort algorithm

---
+++ sorting/Bubblesort.py
@@ -0,0 +1,18 @@
+# This algorithm implement Bubblesort
+
+def bubbleSort ( numbers ):
+    for i in range(0,len(numbers)):
+        for a in range(1,len(numbers)-i):
+            if numbers[a - 1] > numbers[a]:
+                help = numbers[a]
+                numbers[a] = numbers[a-1]
+                numbers[a-1] = help
+    return numbers
+
+
+array = [ 55, 1, 99, 123, 9, 2137, 44, 555, 23, 2, 3, 6 ]
+
+sortedArray = bubbleSort( array )
+
+for number in sortedArray:
+    print( number , end=", " )
