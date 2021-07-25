182feccb92acd62b8a7b414b5dc9894419e1c3b6
Widua <mikolaj.widla@gmail.com>
2021-07-25 14:44:23 +0200

    Added selection sort algorithm

---
+++ sorting/selectSort.py
@@ -0,0 +1,26 @@
+# Algorithm implements selection sort
+
+def selectionSort( numbers ):
+    for i in range (0, len( numbers ) ):
+
+        for a in range( i+1 , len(numbers) ):
+            if numbers[i] > numbers[a]:
+                helpVar = numbers[i]
+                numbers[i] = numbers[a]
+                numbers[a] = helpVar
+    return numbers
+
+
+
+
+
+
+
+
+
+array = [55, 1, 99, 123, 9, 2137, 44, 555, 23, 2, 3, 6]
+
+sortedArray = selectionSort( array )
+
+for number in sortedArray:
+    print( number , end=", " )
